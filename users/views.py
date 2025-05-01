6244be734b1656d25b1b446f95508f10fb266188
Kamil Giszka <kamilgiszka109@gmail.com>
2025-05-01 12:35:06 +0200

    register view and serializer written, login and token refresh url added

--- users/views.py
+++ users/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import CreateUserSerializer
-from users.serializers import CreateUserSerializer
 
 
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
-
-    def perform_create(self, serializer):
-        user = serializer.save()
-        user.set_password(serializer.validated_data['password'])
-        user.save()
--- wiperino/urls.py
+++ wiperino/urls.py
@@ -18,27 +18,43 @@ from django.contrib import admin
 from django.urls import path
 from playerhub import views as playerhub_views
 from livehub import views as livehub_views
+from users import views as users_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/runs/', playerhub_views.RunListView.as_view(), name='runs'),
-    path('api/runs/<int:pk>/', playerhub_views.RunView.as_view(), name='run'),
+    path('api/runs/', playerhub_views.RunListView.as_view(), name='api-runs'),
+    path('api/runs/<int:pk>/', playerhub_views.RunView.as_view(), name='api-run'),
 
-    path('api/runs/<int:run_id>/wipecounters/', playerhub_views.WipeCounterListView.as_view(), name='wipecounters'),
-    path('api/runs/<int:run_id>/wipecounters/<int:wipecounter_id>/', playerhub_views.WipeCounterView.as_view(), name='wipecounter'),
+    path('api/runs/<int:run_id>/wipecounters/',
+         playerhub_views.WipeCounterListView.as_view(), name='api-wipecounters'),
+    path('api/runs/<int:run_id>/wipecounters/<int:wipecounter_id>/',
+         playerhub_views.WipeCounterView.as_view(), name='api-wipecounter'),
 
-    path('api/runs/<int:run_id>/timers/', playerhub_views.TimerListView.as_view(), name='timers'),
-    path('api/runs/<int:run_id>/timers/<int:timer_id>/', playerhub_views.TimerView.as_view(), name='timer'),
+    path('api/runs/<int:run_id>/timers/',
+         playerhub_views.TimerListView.as_view(), name='api-timers'),
+    path('api/runs/<int:run_id>/timers/<int:timer_id>/',
+         playerhub_views.TimerView.as_view(), name='api-timer'),
 
-    path('api/polls/mod/<str:moderator_session_code>/', livehub_views.ModeratorPollListView.as_view(), name='polls-moderator'),
-    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/', livehub_views.ModeratorPollView.as_view(), name='poll-moderator'),
+    path('api/polls/mod/<str:moderator_session_code>/',
+         livehub_views.ModeratorPollListView.as_view(), name='api-polls-moderator'),
+    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/',
+         livehub_views.ModeratorPollView.as_view(), name='api-poll-moderator'),
 
-    path('api/polls/<str:session_code>/', livehub_views.ViewerPollListView.as_view(), name='polls'),
+    path('api/polls/<str:session_code>/',
+         livehub_views.ViewerPollListView.as_view(), name='api-polls'),
 
-    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/answers/', livehub_views.ModeratorAnswerListView.as_view(), name='answers-moderator'),
-    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/answers/<int:answer_id>/', livehub_views.ModeratorAnswerView.as_view(), name='answer-moderator'),
+    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/answers/',
+         livehub_views.ModeratorAnswerListView.as_view(), name='api-answers-moderator'),
+    path('api/polls/mod/<str:moderator_session_code>/<int:poll_id>/answers/<int:answer_id>/',
+         livehub_views.ModeratorAnswerView.as_view(), name='api-answer-moderator'),
 
-    path('api/polls/<str:session_code>/<int:poll_id>/answers/', livehub_views.ViewerAnswerListView.as_view(), name='answers'),
+    path('api/polls/<str:session_code>/<int:poll_id>/answers/',
+         livehub_views.ViewerAnswerListView.as_view(), name='api-answers'),
+
+    path('api/register', users_views.RegisterView.as_view(), name='api-register'),
+    path('api/login', TokenObtainPairView.as_view(), name='api-login'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='api-token-refresh'),
 
 ]
