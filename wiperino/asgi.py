cce7744f3292897b731ed76ad245107dd2942434
Kamil Giszka <kamilgiszka109@gmail.com>
2025-05-05 19:10:44 +0200

    channels configured, custom JWT auth middleware added

--- wiperino/asgi.py
+++ wiperino/asgi.py
@@ -8,17 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from playerhub.middleware import JWTAuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import playerhub.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiperino.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddlewareStack(
         URLRouter(
-            # Websockets routing
+            playerhub.routing.websocket_urlpatterns
         )
     ),
 })
--- wiperino/settings.py
+++ wiperino/settings.py
@@ -92,10 +92,9 @@ ASGI_APPLICATION = "wiperino.asgi.application"
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        # 'CONFIG': {
-        #     "hosts": [("127.0.0.1", 6379)],
-        # }
+        'CONFIG': {
+            "hosts": [("127.0.0.1", 6379)],
+        }
     }
 }
 
