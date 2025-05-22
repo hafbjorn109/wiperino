55b10cae76f3559a1c3cea7ea332708955745b87
Kamil Giszka <kamilgiszka109@gmail.com>
2025-05-22 19:32:44 +0200

    routing.py cleaned up, readme updated

--- playerhub/routing.py
+++ playerhub/routing.py
@@ -5,7 +5,6 @@ websocket_urlpatterns = [
     re_path(r'ws/runs/(?P<run_id>\d+)/$', consumers.WipecounterConsumer.as_asgi()),
     re_path(r'ws/overlay/runs/(?P<run_id>\d+)/$', consumers.OverlayConsumer.as_asgi()),
     re_path(r'ws/polls/(?P<client_token>[\w\-]+)/$', consumers.PollConsumer.as_asgi()),
-    re_path(r'ws/polls/(P<client_token>[\w\-]+)/$', consumers.PollConsumer.as_asgi()),
     re_path(r'ws/runs/(?P<run_id>\d+)/timer/$', consumers.TimerConsumer.as_asgi()),
     re_path(r'ws/overlay/runs/(?P<run_id>\d+)/timer/$', consumers.OverlayTimerConsumer.as_asgi()),
 ]
